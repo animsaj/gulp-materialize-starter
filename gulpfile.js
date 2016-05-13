a9692b85508762865c914a1b7144ca128c958e9a
animsaj <animsaj70@gmail.com>
2016-05-13 23:31:25 +0300

    adding README to images folder && correcting some typos

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var browserSync = require('browser-sync').create();
+var browserSync = require('browser-sync');
 var useref = require('gulp-useref');
 var uglify = require('gulp-uglify');
 var gulpIf = require('gulp-if');
@@ -8,14 +8,14 @@ var cssnano = require('gulp-cssnano');
 var imagemin = require('gulp-imagemin');
 var cache = require('gulp-cache');
 var del = require('del');
-var runSequence = require('run-sequence');
+var runSequence = require('run-sequence').use(gulp);
 
-gulp.task('browser-synk', function() {
-    browserSync.init({
-        server: {
-            baseDir: 'app'
-        }
-    });
+gulp.task('browserSync', function() {
+  browserSync({
+    server: {
+      baseDir: 'app'
+    }
+  });
 });
 
 gulp.task('sass', function() {
